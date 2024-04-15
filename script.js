f19d7b2c2cb0c3bcb8cbc2ed21099af1c89ae561
Deepu <deepukumarrao632@gmail.com>
2024-04-15 21:05:15 +0530

    working

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let isLocked = false;
 
+// Copy function element
 function copyCode() {
   const textarea = document.getElementById("textBox");
   textarea.select();
   document.execCommand("copy");
-  alert("code copy to clickboard!");
+  alert("Code copy to clickboard!");
 }
-
+// Saved function element
 function saveCode() {
   const textBox = document.getElementById("textBox").value;
   const blob = new Blob([textBox], { type: "text/plain" });
@@ -16,13 +17,14 @@ function saveCode() {
   link.download = "textBox.txt";
   link.click();
   URL.revokeObjectURL(url);
-  alert("code saved");
+  alert("code saved!");
 }
-
+// Lock and unlock function element
 function toggleLock() {
   isLocked = !isLocked;
   const textarea = document.getElementById("textBox");
   const lockBtn = document.querySelector(".container__button--lock");
+
   if (isLocked) {
     textarea.setAttribute("readonly", true);
     lockBtn.classList.add("active");
